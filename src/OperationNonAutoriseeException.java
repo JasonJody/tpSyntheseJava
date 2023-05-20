e243b87ddd8233d4aa590b326c618e80a426f8fc
Jason Jody Aube <jasonjody2005@gmail.com>
2023-05-20 17:16:33 -0400

    TODO 9

--- src/OperationNonAutoriseeException.java
+++ src/OperationNonAutoriseeException.java
@@ -1,2 +1,7 @@
 // Exception personnalisée pour une opération non autorisée
 //TODO 9
+public class OperationNonAutoriseeException extends Exception {
+    public OperationNonAutoriseeException(String message) {
+        super(message);
+    }
+}
