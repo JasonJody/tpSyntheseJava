b78fa51b92f806f9709f1d305aa1083098e4fcde
Jason Jody Aube <jasonjody2005@gmail.com>
2023-05-20 16:19:28 -0400

    TODO 2

--- src/Footballeur.java
+++ src/Footballeur.java
@@ -1,6 +1,12 @@
 
 // Classe dérivée Footballeur
 class Footballeur extends Joueur {
+
+    private String equipe;
+    public Footballeur(String nom, int age, String equipe) {
+        super(nom, age);
+        this.equipe = equipe;
+    }
     // TODO 2
 
     // Implémentation de la méthode abstraite pour afficher les détails du footballeur, incluant l'équipe
@@ -10,6 +16,15 @@ class Footballeur extends Joueur {
         System.out.println("Âge: " + getAge());
         System.out.println("Équipe: " + equipe);
     }
+
+    public void setEquipe(String equipe) {
+        this.equipe = equipe;
+    }
+
+    public String getEquipe() {
+        return equipe;
+    }
+
 }
 
 
