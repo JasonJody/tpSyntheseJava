dc6321f7aab1c89fda425f15e2cb2f8395fd8bd1
Jason Jody Aube <jasonjody2005@gmail.com>
2023-05-20 16:40:21 -0400

    TODO 6

---
+++ src/Noeud.java
@@ -0,0 +1,11 @@
+public class Noeud {
+
+    protected Noeud suivant;
+    protected Noeud precedent;
+    protected int valeur;
+
+    public Noeud (int valeur) {
+        this.valeur = valeur;
+    }
+
+}
